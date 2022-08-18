1c2278a61498f9f6d09cee0a2ee1634583ef8d99
LHpanika <kochanov.p42@gmail.com>
2022-08-18 20:21:22 +0300

    added task28

---
+++ task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, 
+// которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+Console.WriteLine("Enter number N");
+int N = Convert.ToInt32(Console.ReadLine());
+int fact = 1;
+if (N>=1)
+{
+    for (int i=1; i<=N; i++)
+    {
+    fact = fact * i;
+    }
+    Console.WriteLine(fact);
+}
+else
+{
+Console.WriteLine("Введено некорректное значение N");
+}
