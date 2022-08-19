118989514b43c172bd20d61355f52f4e88f1a925
LHpanika <kochanov.p42@gmail.com>
2022-08-19 18:02:24 +0300

    added task29 - HOMEWORK

---
+++ task29-HW/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов 
+// и выводит их на экран. 
+// Вводим массив через консоль
+int[] arr = new int[8];
+Console.WriteLine("Введите восемь чисел");
+for (int i=1; i<arr.Length; i++)
+{
+    arr[i]=Convert.ToInt32(Console.ReadLine());
+}
+//Console.WriteLine("CREATED");
+Console.Write(string.Join(", ", arr));
